79af63186f9b892cfcbd8190c960d4af92728062
James M. Joyce <james@flashpointcs.net>
2019-03-21 20:25:48 -0500

    Skeleton

---
+++ src/android/NotificationSettings.java
@@ -0,0 +1,9 @@
+package net.flashpointcs.cordova.notificationsettings;
+
+import org.apache.cordova.CordovaPlugin;
+
+public class NotificationSettings extends CordovaPlugin {
+
+   private final string PLUGIN = "NotificationSettings";
+
+}
